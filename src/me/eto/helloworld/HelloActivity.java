fa91b8090026eda2ef53c3b09ab182a6cc4b4dd6
eliantor <eliantor@gmail.com>
2013-05-08 13:22:09 +0200

    Send values accross activities

--- src/me/eto/helloworld/HelloActivity.java
+++ src/me/eto/helloworld/HelloActivity.java
@@ -7,25 +7,36 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class HelloActivity extends Activity{
 	private final static String TAG = "HELLO";
+	
+	private EditText mInputName;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.start_layout);
 		Button submit = (Button)findViewById(R.id.btn_submit);
+		mInputName = (EditText)findViewById(R.id.edt_name);
+		
 		submit.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				Log.wtf(TAG, "Clicked!");
+				String name = getText();
 				final Intent intent = new Intent(HelloActivity.this, 
 												 HelloResultActivity.class);
+				intent.putExtra(HelloResultActivity.EXTRA_NAME_ARGUMENT, name);
 				startActivity(intent);
 			}
 		});
 	}
 	
-	
+	private String getText(){
+		String string = mInputName.getText().toString();
+		Log.i(TAG, "Input is: "+string);
+		return string;
+	}
 }
--- src/me/eto/helloworld/HelloResultActivity.java
+++ src/me/eto/helloworld/HelloResultActivity.java
@@ -1,13 +1,25 @@
 package me.eto.helloworld;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HelloResultActivity extends Activity{
-
+	public static String EXTRA_NAME_ARGUMENT = "name";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		setContentView(R.layout.hello_layout);
+		
+		Intent theIntentThatStartedUs = getIntent();
+		String name = theIntentThatStartedUs.getStringExtra(EXTRA_NAME_ARGUMENT);
+		
+		if(name != null && name.length()>0){
+			TextView tv = (TextView)findViewById(R.id.tv_result);
+			tv.setText(String.format("Hello, %s!", name));
+		}
 	}
 }
