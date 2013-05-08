6df449938ad8c6e3f5d02c1388768753ffd11c0f
eliantor <eliantor@gmail.com>
2013-05-08 12:24:04 +0200

    first commit

---
+++ src/me/eto/helloworld/HelloActivity.java
@@ -0,0 +1,15 @@
+package me.eto.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelloActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.hello_layout);
+	}
+	
+	
+}
