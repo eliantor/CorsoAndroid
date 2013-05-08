ec6d4e377cdefe57af71b02e654adbffb49d3d71
eliantor <eliantor@gmail.com>
2013-05-08 13:01:32 +0200

    Buttons and startActivity

--- src/me/eto/helloworld/HelloActivity.java
+++ src/me/eto/helloworld/HelloActivity.java
@@ -1,14 +1,30 @@
 package me.eto.helloworld;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class HelloActivity extends Activity{
-
+	private final static String TAG = "HELLO";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.hello_layout);
+		setContentView(R.layout.start_layout);
+		Button submit = (Button)findViewById(R.id.btn_submit);
+		submit.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Log.wtf(TAG, "Clicked!");
+				final Intent intent = new Intent(HelloActivity.this, 
+												 HelloResultActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 	
 	
---
+++ src/me/eto/helloworld/HelloResultActivity.java
@@ -0,0 +1,13 @@
+package me.eto.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelloResultActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.hello_layout);
+	}
+}
